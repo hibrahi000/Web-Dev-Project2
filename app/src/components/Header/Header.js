33adf70dd5a965c48f3981517b395fd99da6deb3
Mark Ammari <mark.ammari@gmail.com>
2019-05-08 13:06:32 -0400

    added Header and installed materialUI

---
+++ app/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Header.css';
+
+const Header = (props) => {
+    return (
+        <div className={classes.Header}>
+            <p>logo</p>
+            <p>Navbar</p>
+            <p>buttons</p>
+        </div>
+    );
+};
+
+export default Header;
