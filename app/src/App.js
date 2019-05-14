342a22d7e05ee22a98f89c0ebb0ff310e7629743
Mark Ammari <mark.ammari@gmail.com>
2019-05-14 15:49:17 -0400

    added routing to Contact and About page

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,8 @@ import Layout from './components/Layout/Layout'
 import Footer from './components/Footer/Footer'
 import Home from './pages/Home'
 import Catagory from './pages/Catagory'
+import Contact from './pages/Contact'
+import About from './pages/About'
 import { BrowserRouter, Route } from 'react-router-dom'
 class App extends Component {
   render() {
@@ -11,8 +13,10 @@ class App extends Component {
       <Layout>
         <Header />
           <BrowserRouter>
-          <Route path="/" component={Home} exact />
-          <Route path="/Catagory" component={Catagory} />
+            <Route path="/" component={Home} exact />
+            <Route path="/Catagory" component={Catagory} />
+            <Route path="/Contact" component={Contact} />
+            <Route path="/About" component={About} />
           </BrowserRouter>
         <Footer />
       </Layout>
---
+++ app/src/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Layout from '../components/Layout/Layout'
+
+class About extends Component {
+  render() {
+    return (
+      <Layout>
+      </Layout>
+    );
+  }  
+}
+
+export default About;
